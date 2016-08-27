f475d4069f974d36c8dea661f8bded7302de4aa4
Anthony Eden <me@anthonyeden.com>
2016-08-27 16:25:01 +0200

    Correct init argument name

--- ruby/badauth.rb
+++ ruby/badauth.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 require 'dnsimple'
-client = Dnsimple::Client.new(base_uri: "https://api.sandbox.dnsimple.com", access_token: "fake")
+client = Dnsimple::Client.new(base_url: "https://api.sandbox.dnsimple.com", access_token: "fake")
 response = client.identity.whoami
 puts response.inspect
