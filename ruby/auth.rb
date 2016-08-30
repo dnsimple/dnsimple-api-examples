70a569c8ceec514d3960ea08990089da68c226c4
Anthony Eden <me@anthonyeden.com>
2016-08-30 15:43:25 +0200

    More pedagogy by comments

--- ruby/auth.rb
+++ ruby/auth.rb
@@ -2,6 +2,17 @@
 require 'pp'
 require 'dnsimple'
 require_relative 'token'
+
+# Construct a client instance.
+#
+# If you want to connect to production, omit the `base_url` option.
 client = Dnsimple::Client.new(base_url: "https://api.sandbox.dnsimple.com", access_token: TOKEN)
+
+# All calls to client pass through a service. In this case, `client.identity` is the identity service.
+#
+# Dnsimple::Client::Identity#whoami is the method for retrieving the account details for your
+# current credentials via the DNSimple API.
 response = client.identity.whoami
+
+# Pretty-print the entire response object so you can see what is inside.
 pp response
