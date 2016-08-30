35d5caa27d793ec9b49e912ab28a47dd98890006
Anthony Eden <me@anthonyeden.com>
2016-08-30 15:48:04 +0200

    Domains listing example

---
+++ ruby/domains.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+require 'pp'
+require 'dnsimple'
+require_relative 'token'
+
+# Construct a client instance.
+#
+# If you want to connect to production, omit the `base_url` option.
+client = Dnsimple::Client.new(base_url: "https://api.sandbox.dnsimple.com", access_token: TOKEN)
+
+# All calls to client pass through a service. In this case, `client.identity` is the identity service.
+#
+# Dnsimple::Client::Identity#whoami is the method for retrieving the account details for your
+# current credentials via the DNSimple API.
+response = client.identity.whoami
+
+# The response object returned by any API method includes a `data` attribute. Underneath that
+# attribute is an attribute for each data object returned. In this case, `#account` provides
+# access to the resulting account object.
+#
+# Here the account ID is extracted for use in the call to list domains.
+account_id = response.data.account.id
+
+# Dnsimple::Client::Domains#domains is the method for retrieving a page from the list of domains
+# in an account. The default page size is 30 domains. If your account has more than 30 domains then
+# you can page through the results with repeated calls to `#domains` or you can use the `#all_domains`
+# method for automatic paging.
+#
+# In this example we simply retrieve the first page of domains.
+response = client.domains.domains(account_id)
+
+# Pretty-print the entire response object so you can see what is inside.
+pp response
