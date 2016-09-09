4d4b37046aed50888354c790b00effb9d32e5b3a
Sebastian Hermida <sebas00@gmail.com>
2016-09-09 11:23:19 +0200

    Adds elixir example on how to get DNS records for a zone

---
+++ elixir/zones.exs
@@ -0,0 +1,21 @@
+HTTPoison.start
+
+client = %Dnsimple.Client{access_token: Application.get_env(:dnsimple, :access_token), base_url: "https://api.sandbox.dnsimple.com/"}
+
+# Grabs the first account based on your user token
+account = case Dnsimple.Accounts.accounts(client) do
+  {:ok, response} -> List.first(response.data)
+  {:error, error} -> raise RuntimeError, message: error.message
+end
+
+# Grabs the first domain on this account
+domain = case Dnsimple.Domains.domains(client, account.id) do
+  {:ok, response} -> List.first(response.data)
+  {:error, error} -> raise RuntimeError, message: error.message
+end
+
+# List all the DNS records for this domain
+case Dnsimple.Zones.records(client, account.id, domain.name) do
+  {:ok, response} -> IO.inspect response.data
+  {:error, error} -> raise RuntimeError, message: error.message
+end
