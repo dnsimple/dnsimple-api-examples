de2bef5a0e29f83f1a92de013ad594cfc2ed797c
Sebastian Hermida <sebas00@gmail.com>
2016-09-09 12:18:03 +0200

    Uses the zone instead of the domain module

--- elixir/zone_records.exs
+++ elixir/zone_records.exs
@@ -8,14 +8,14 @@ account = case Dnsimple.Accounts.accounts(client) do
   {:error, error} -> raise RuntimeError, message: error.message
 end
 
-# Grabs the first domain on this account
-domain = case Dnsimple.Domains.domains(client, account.id) do
+# Grabs the first zone on this account
+zone = case Dnsimple.Zones.zones(client, account.id) do
   {:ok, response} -> List.first(response.data)
   {:error, error} -> raise RuntimeError, message: error.message
 end
 
 # List all the DNS records for this domain
-case Dnsimple.Zones.records(client, account.id, domain.name) do
+case Dnsimple.Zones.records(client, account.id, zone.name) do
   {:ok, response} -> IO.inspect response.data
   {:error, error} -> raise RuntimeError, message: error.message
 end
