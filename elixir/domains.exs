10fae290d74edd08929fdd3b845dfe5e1aeb5c09
Anthony Eden <me@anthonyeden.com>
2016-09-20 07:41:17 +0200

    Elixir list domains example

---
+++ elixir/domains.exs
@@ -0,0 +1,8 @@
+HTTPoison.start
+
+client = %Dnsimple.Client{access_token: Application.get_env(:dnsimple, :access_token), base_url: "https://api.sandbox.dnsimple.com/"}
+
+{:ok, response} = Dnsimple.Identity.whoami(client)
+account_id = response.data.account["id"]
+
+IO.inspect Dnsimple.Domains.list_domains(client, account_id)
