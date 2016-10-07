cdba5415d462b8f2c9fe6f9c4d54fa3f9ef1d7ba
Anthony Eden <me@anthonyeden.com>
2016-10-07 17:18:06 +0200

    Use Identity.whoami to get the account and use a passed in domain name for listing zone records

--- elixir/zone_records.exs
+++ elixir/zone_records.exs
@@ -2,22 +2,21 @@
 
 Dnsimple.start
 
-client = %Dnsimple.Client{access_token: Application.get_env(:dnsimple, :access_token), base_url: "https://api.sandbox.dnsimple.com/"}
+client = %Dnsimple.Client{
+  access_token: Application.get_env(:dnsimple, :access_token),
+  base_url: "https://api.sandbox.dnsimple.com/"
+}
 
 # Grabs the first account based on your user token
-account = case Dnsimple.Accounts.accounts(client) do
-  {:ok, response} -> List.first(response.data)
+account = case Dnsimple.Identity.whoami(client) do
+  {:ok, response} -> response.data.account
   {:error, error} -> raise RuntimeError, message: error.message
 end
 
-# Grabs the first zone on this account
-zone = case Dnsimple.Zones.zones(client, account.id) do
-  {:ok, response} -> List.first(response.data)
-  {:error, error} -> raise RuntimeError, message: error.message
-end
+name = List.last(System.argv)
 
-# List all the DNS records for this domain
-case Dnsimple.Zones.records(client, account.id, zone.name) do
+# List all the DNS records for the given domain
+case Dnsimple.Zones.list_zone_records(client, account["id"], name) do
   {:ok, response} -> IO.inspect response.data
   {:error, error} -> raise RuntimeError, message: error.message
 end
