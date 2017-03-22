ac1490b856435bd9eb82b08bceda0b63b794ceda
Luca Guidi <me@lucaguidi.com>
2017-03-22 12:32:13 +0100

    Added Go example for domain registration

---
+++ golang/cmd/register_domain/register_domain.go
@@ -0,0 +1,48 @@
+// Usage: TOKEN=token go run cmd/register_domain/register_domain.go example.com
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/dnsimple/dnsimple-go/dnsimple"
+)
+
+func main() {
+	oauthToken := os.Getenv("TOKEN")
+	client := dnsimple.NewClient(dnsimple.NewOauthTokenCredentials(oauthToken))
+	client.BaseURL = "https://api.sandbox.dnsimple.com"
+
+	// get the current authenticated account (if you don't know who you are)
+	whoamiResponse, err := client.Identity.Whoami()
+	if err != nil {
+		fmt.Printf("Whoami() returned error: %v\n", err)
+		os.Exit(1)
+	}
+
+	accountId := strconv.Itoa(whoamiResponse.Data.Account.ID)
+
+	domainName := os.Args[1]
+	fmt.Printf("Registering domain %v\n", domainName)
+	contacts, err := client.Contacts.ListContacts(accountId, nil)
+
+	if err != nil {
+		fmt.Printf("ListContacts() returned error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if len(contacts.Data) == 0 {
+		fmt.Printf("ListContacts() returned no contacts. Please create first a contact.")
+		os.Exit(1)
+	}
+
+	contact := contacts.Data[0]
+
+	registerDomainResponse, err := client.Registrar.RegisterDomain(accountId, domainName, &dnsimple.DomainRegisterRequest{RegistrantID: contact.ID})
+	if err != nil {
+		fmt.Printf("RegisterDomain() returned error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%+v\n", registerDomainResponse.Data)
+}
