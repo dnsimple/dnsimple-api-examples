2f595eb00ccc5c6f3869ca034a44648ea3d493a8
DXTimer <ibbakalov@gmail.com>
2021-02-22 21:53:47 +0200

    Update: add namespacing and update precedence

--- ruby/use_cases/issue_certificates/config.ru
+++ ruby/use_cases/issue_certificates/config.ru
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
+ENV['APP_DIR'] ||= File.expand_path(__dir__)
+
 require File.expand_path('lib/app', __dir__)
 
 require 'app/web'
-
-ENV['APP_DIR'] ||= File.expand_path(__dir__)
 
 run App::Web
--- ruby/use_cases/issue_certificates/lib/app/handlers/certificate_issue_event_handler.rb
+++ ruby/use_cases/issue_certificates/lib/app/handlers/certificate_issue_event_handler.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
-module CertificateIssueEventHandler
-  module_function
+module App
+  module Handlers
+    module CertificateIssueEventHandler
+      module_function
 
-  def handle(event)
-    certificate = App::DnsimpleAdapter.download_certificate(
-      event.data['certificate']['domain_id'],
-      event.data['certificate']['id']
-    )
-    private_key = App::DnsimpleAdapter.certificate_private_key(
-      event.data['certificate']['domain_id'],
-      event.data['certificate']['id']
-    ).private_key
+      def handle(event)
+        certificate = App::DnsimpleAdapter.download_certificate(
+          event.data['certificate']['domain_id'],
+          event.data['certificate']['id']
+        )
+        private_key = App::DnsimpleAdapter.certificate_private_key(
+          event.data['certificate']['domain_id'],
+          event.data['certificate']['id']
+        ).private_key
 
-    p certificate
-    p private_key
-    # Now you can carry out your business logic of pushing the certificate to
-    # your infrastructure automation tool and secrets store.
+        # Now you can carry out your business logic of pushing the certificate to
+        # your infrastructure automation tool and secrets store.
+      end
+    end
   end
 end
