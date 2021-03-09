57129e1e4fcc81825ce9d69aa6416bd38ed25116
DXTimer <ibbakalov@gmail.com>
2021-03-09 11:42:18 +0200

    /issue_certificate endpoint has been added

--- ruby/use_cases/issue_certificates/lib/app/dnsimple.rb
+++ ruby/use_cases/issue_certificates/lib/app/dnsimple.rb
@@ -12,5 +12,27 @@ module App
     post '/events' do
       App::Consumers::DnsimpleEventConsumer.process(params.to_h)
     end
+
+    post '/issue_certificate' do
+      certificate_order = App::DnsimpleAdapter.purchase_letsencrypt_certificate(
+        parsed_request_body['domain'],
+        App.config.dnsimple['contact_id'],
+        name: parsed_request_body['name'],
+        alternate_names: parsed_request_body['san']&.split(','),
+        auto_renew: parsed_request_body.fetch('auto_renew', true)
+      )
+      certificate = App::DnsimpleAdapter.issue_letsencrypt_certificate(parsed_request_body['domain'],
+                                                                       certificate_order.certificate_id)
+
+      json(data: {
+             certificate_id: certificate.id,
+             common_name: certificate.common_name,
+             san: certificate.alternate_names,
+             state: certificate.state,
+             auto_renew: certificate.auto_renew
+           })
+    rescue ::Dnsimple::NotFoundError, ::Dnsimple::RequestError => e
+      halt 400, json(error: { message: e.message })
+    end
   end
 end
