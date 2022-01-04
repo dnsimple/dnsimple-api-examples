2796fc483f4449b76df3803807efbe1b98e98b1d
Enrique Comba Riepenhausen <enrique@ecomba.pro>
2022-01-04 10:03:14 +0100

    Adds account_list, auth, bad_auth and start of cancel_domain_transfer samples

---
+++ rust/src/bad_auth.rs
@@ -0,0 +1,20 @@
+use dnsimple::dnsimple::new_client;
+
+pub fn bad_auth() {
+    // Construct a client instance
+    //
+    // If you want to connect to production set the sandbox argument to false
+    //
+    // Note that in this case a bogus access token is being sent to show what happens when
+    // authentication fails.
+    let client = new_client(true, "fake".into());
+
+    // All calls to client pass through a service. In this case, `client.identity()` is the identity
+    // service.
+    //
+    // `client.identity().whoami` is the method for retrieving the account details for your
+    // current credentials via the DNSimple API.
+    let response = client.identity().whoami();
+
+    println!("The server replied with: {:?}", response)
+}
