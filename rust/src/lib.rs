89265ca7746418cb22e06d7984472b49df5457a6
Amelia Aronsohn <squirrel@wearing.black>
2022-07-27 14:03:32 -0700

    Clean up lib

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,6 @@
 use std::fs;
 
 pub fn token_from_file() -> String {
-    let mut token = fs::read_to_string("./token.txt")
-        .unwrap()
-        .parse::<String>()
-        .unwrap();
-    let len = token.len();
-    token.truncate(len - 1);
-    token
+    let token: String = fs::read_to_string("./token.txt").unwrap();
+    token.trim().to_string()
 }
